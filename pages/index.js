474955e846f6e6dfa114b37a24defb6ea94084e1
Esteban-Pena <89949341+Esteban-Pena@users.noreply.github.com>
2023-05-23 14:33:09 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function Home({colors}){
+  return(
+    <div>
+    {colors.length &&
+    colors.map((color, index) => (
+      <Link
+      href = {{
+        pathname: `/${color.name}&${color.hex}`,
+      }}
+
+      key={index}
+      >
+      <h2>{color.name}</h2>
+      </Link>
+    ))}
+    </div>
+  )
+}  
+
+export const getServerSideProps = async () =>{
+  let res = await fetch("http://localhost:3000/api/colors", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let colors = await res.json();
+  return {
+    props: { colors },
+  };
+};
+
